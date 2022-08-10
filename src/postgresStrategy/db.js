36861e8e1ec71fb5178c28792957cba0593e4d2c
KayckSchuenck <ycferreiras@gmail.com>
2022-08-10 19:54:39 -0300

    Erro de build corrigido

--- src/postgresStrategy/db.js
+++ src/postgresStrategy/db.js
@@ -2,20 +2,13 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-//const databaseConfig = {
-  //  connectionString: process.env.DATABASE_URL,
-    //ssl: {
-      //  rejectUnauthorized: false
-    //}
-//}
-
-const databaseConfig={
-    user:"postgres",
-    host:"localhost",
-    database:"teste",
-    password:"keening123",
-    port:5432
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
+
 
 const db = new Pool(databaseConfig);
 
