caae96b83fad11f44d333e3ea238a90b8ed3ec96
KayckSchuenck <ycferreiras@gmail.com>
2022-08-11 19:15:48 -0300

    Heroku is back

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,21 +1,19 @@
-import bcrypt from 'bcrypt'
-import { authRepository } from '../repositories/authRepository.js'
+import bcrypt from "bcrypt";
+import { authRepository } from "../repositories/authRepository.js";
 
-export async function sign_up(req,res) {
-    const {username,email,password,pictureURL}=req.body
-    
-    try{
-        const {rows:checkEmail}=await authRepository.getUser(email)
-        if(checkEmail.length!==0) return res.status(409).send("Usuario já cadastrado")
+export async function sign_up(req, res) {
+  const { username, email, password, pictureURL } = req.body;
 
-        const hashPassword=bcrypt.hashSync(password, 10);
-        await authRepository.postSignUp(email,hashPassword,username,pictureURL)
+  try {
+    const { rows: checkEmail } = await authRepository.getUser(email);
+    if (checkEmail.length !== 0)
+      return res.status(409).send("Usuario já cadastrado");
 
-        res.sendStatus(201)
+    const hashPassword = bcrypt.hashSync(password, 10);
+    await authRepository.postSignUp(email, hashPassword, username, pictureURL);
 
-    } catch(e){
-        console.log(e)
-        res.status(500).send("Erro com o servidor")
-    }
-    
+    res.sendStatus(201);
+  } catch (e) {
+    res.status(500).send("Erro com o servidor");
+  }
 }
--- src/postgresStrategy/db.js
+++ src/postgresStrategy/db.js
@@ -1,19 +1,18 @@
-import pg from 'pg';
+import pg from "pg";
 
 const { Pool } = pg;
 
 const databaseConfig = {
-    //connectionString: process.env.DATABASE_URL,
-    user: "postgres",
-    password: "12345678J" ,
-    host: "localhost",
-    port: "5432",
-    database: "projetolinkr",
-    ssl: {
-        rejectUnauthorized: false
-    }
-}
-
+  connectionString: process.env.DATABASE_URL,
+  // user: "postgres",
+  // password: "12345678J" ,
+  // host: "localhost",
+  // port: "5432",
+  // database: "projetolinkr",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 const db = new Pool(databaseConfig);
 
