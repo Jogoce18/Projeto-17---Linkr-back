af0d343663299e2f6b03164b43aacd29e7d7ad8b
KayckSchuenck <ycferreiras@gmail.com>
2022-08-15 12:21:09 -0300

    Fix

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -1,33 +1,32 @@
 import { hashtagsRepository } from "../repositories/hashtagsRepository.js";
 
+export async function getHashTags(req, res) {
+  try {
+    const result = await hashtagsRepository.getTrending();
+    if (result.rowCount === 0)
+      return res.send("Ainda nao existe trending ").status(404);
 
-
-export async function getHashTags(req,res){
-
-    try {
-        const result = await hashtagsRepository.getTrending()
-        if(result.rowCount === 0)return res.send("Ainda nao existe trending ").status(404)
-
-        res.send(result.rows).status(200)
-      
-    } catch (error) {
-        console.log(error)
-        res.sendStatus(500)
-    }
-   
-
+    res.send(result.rows).status(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
 export async function getHashtagByName(req, res) {
   const { hashtag } = req.params;
-  
 
   try {
-    const result = await hashtagsRepository.getHashtagPosts(hashtag);
-    console.log(result)
-    if (result.rowCount == 0) return res.sendStatus(404);
-    console.log(result.rows)
-    res.send(result.rows).status(200);
+    const { rows: result } = await hashtagsRepository.getHashtagPosts(hashtag);
+    if (result.length === 0) return res.sendStatus(404);
 
+    const { rows: likes } = await likesRepository.getLikes();
+
+    const joinHashtags = result.map((post) => {
+      const filterLikes = likes.filter((like) => like.postId === result.postId);
+      return { ...post, likes: filterLikes };
+    });
+
+    res.send(joinHashtags).status(200);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
