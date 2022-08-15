2170dbbb96ccd95afeea65794f72f34cf4ab2c3e
KayckSchuenck <ycferreiras@gmail.com>
2022-08-15 00:40:47 -0300

    Fix

--- src/schemas/userPostSchema.js
+++ src/schemas/userPostSchema.js
@@ -2,6 +2,6 @@ import joi from "joi";
 const postSchema = joi.object({
   url: joi.string().uri().required(),
   article: joi.string().required(),
-  hashtags: joi.string().required(),
+  hashtags: joi.array().required(),
 });
 export default postSchema;
